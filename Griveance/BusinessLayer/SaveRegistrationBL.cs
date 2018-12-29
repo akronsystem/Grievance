ed1c95b4e639a7af49b90066dc446e6c8a536c2c
akronsystem <support@akronsystems.com>
2018-12-29 17:04:25 +0530

    Changes 29 Dec 2018

--- Griveance/BusinessLayer/SaveRegistrationBL.cs
+++ Griveance/BusinessLayer/SaveRegistrationBL.cs
@@ -59,7 +59,7 @@ namespace Griveance.BusinessLayer
                     tbl_parent objparent = new tbl_parent();
                     objparent.UserId = PR.UserId;
                     objparent.relationship = PR.Relationship;
-                    tbl_user obstudent = db.tbl_user.Where(r => r.code == PR.Code).FirstOrDefault();
+                    tbl_user obstudent = db.tbl_user.Where(r => r.UserId == PR.UserId).FirstOrDefault();
                     objparent.UserId = obstudent.UserId;
                     db.tbl_parent.Add(objparent);
                     db.SaveChanges(); 
