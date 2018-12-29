567eceabd5ce419b65b37134087df4e584f1d013
bhagyshreetaralekar <45756217+bhagyshreetaralekar@users.noreply.github.com>
2018-12-29 18:02:06 +0530

    Grievance Changes

--- Griveance/Scripts/ProjectScript/Staff/StaffController.js
+++ Griveance/Scripts/ProjectScript/Staff/StaffController.js
@@ -48,7 +48,7 @@ function UsersController($scope, Service, DTOptionsBuilder) {
 
         })
 
-    } 
+    }
 
     $scope.ShowHide = function (UserId) {
 
@@ -119,54 +119,55 @@ function UsersController($scope, Service, DTOptionsBuilder) {
             $scope.Clear();
             $scope.IsVisible = false;
             $scope.Initialize();
-        }); 
-    $scope.GetInfo = function () {
-        $scope.dtOptions = "";
-        Service.Post("api/Grievance/GriveanceTypeInfo", $scope.UserCredentialModel).then(function (result) {
-            debugger;
-            // $scope.ParamUserLogin.Name = result.data.Name
-            $scope.tbl_grievance_list = result.data;
-            $scope.Grievance = result.data.ResultData;
-            console.log(result.data);
-
-        })
-
-    }
-    $scope.SavePost = function () {
-        var data = sessionStorage.getItem('userid');
-        var Password = sessionStorage.getItem('Password');
-        var code = sessionStorage.getItem('emp-key');
-        var email = sessionStorage.getItem('Email');
-        var type = sessionStorage.getItem('Type');
-        PostGr = {
-            GriveanceType: $scope.GriveanceType,
-            Subject: $scope.Subject,
-            PostDetails: $scope.PostDetails,
-            UserId: data,
-            Password: Password,
-            Code: code,
-            Email: email,
-            Type: type
-        };
-        Service.Post("api/Grievance/PostGrievance", JSON.stringify(PostGr)).then(function (result) {
-            debugger;
-            // $scope.ParamUserLogin.Name = result.data.Name
-
-            console.log(result.data);
-            console.log(result.data);
-            if (result.data.IsSucess) {
+        });
+        $scope.GetInfo = function () {
+            $scope.dtOptions = "";
+            Service.Post("api/Grievance/GriveanceTypeInfo", $scope.UserCredentialModel).then(function (result) {
                 debugger;
+                // $scope.ParamUserLogin.Name = result.data.Name
+                $scope.tbl_grievance_list = result.data;
+                $scope.Grievance = result.data.ResultData;
                 console.log(result.data);
-                window.location = "./StaffGrievance"
-            }
-            else {
-                alert(result.data.Message);
-                window.location = "./PostGrievance"
-            }
 
-        }) 
+            })
+
+        }
+        $scope.SavePost = function () {
+            var data = sessionStorage.getItem('userid');
+            var Password = sessionStorage.getItem('Password');
+            var code = sessionStorage.getItem('emp-key');
+            var email = sessionStorage.getItem('Email');
+            var type = sessionStorage.getItem('Type');
+            PostGr = {
+                GriveanceType: $scope.GriveanceType,
+                Subject: $scope.Subject,
+                PostDetails: $scope.PostDetails,
+                UserId: data,
+                Password: Password,
+                Code: code,
+                Email: email,
+                Type: type
+            };
+            Service.Post("api/Grievance/PostGrievance", JSON.stringify(PostGr)).then(function (result) {
+                debugger;
+                // $scope.ParamUserLogin.Name = result.data.Name
+
+                console.log(result.data);
+                console.log(result.data);
+                if (result.data.IsSucess) {
+                    debugger;
+                    console.log(result.data);
+                    window.location = "./StaffGrievance"
+                }
+                else {
+                    alert(result.data.Message);
+                    window.location = "./PostGrievance"
+                }
+
+            })
+        }
+
     }
-  
 }
 //function Users($scope, Service) {
 //    $scope.GetInfo = function () {
